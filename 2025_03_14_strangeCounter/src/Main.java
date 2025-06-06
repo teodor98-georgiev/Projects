f281bffc89e8ba96ea493b5fb00a6cd839b7573b
teodor98-georgiev <54015566+teodor98-georgiev@users.noreply.github.com>
2025-06-06 15:22:25 +0300

    Update Main.java

--- 2025_03_14_strangeCounter/src/Main.java
+++ 2025_03_14_strangeCounter/src/Main.java
@@ -4,14 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int initialNumber = 3;
-        int caseNumber = 3;
-        int caseDecreased = 0;
+        int caseNumber = 0;
+        caseNumber = initialNumber;
         int timeInput = scan.nextInt();
         if (timeInput >= 1 && timeInput <= Math.pow(10,12)){
-            for (int i = 2; i < timeInput; i++){
-                caseNumber = caseNumber - 1;
-                if (caseNumber == 1){
-                    caseNumber = initialNumber * 2;
+            for (int i = 1; i <= timeInput; i++){
+                if (i == 1){
+                    continue;
+                }
+                if (caseNumber != 1){
+                    caseNumber = caseNumber - 1;
+                }
+                else  {
+                    initialNumber = initialNumber * 2;
+                    caseNumber = initialNumber;
                 }
             }
             System.out.println(caseNumber);
